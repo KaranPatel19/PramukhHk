6d83bdf6d28b214b0e71a29578856fe0eb2dd3c1
Hardi503 <106682142+Hardi503@users.noreply.github.com>
2025-06-23 16:51:00 -0400

    Add files via upload

---
+++ alldata.js
@@ -0,0 +1,742 @@
+const players = [
+  {
+    "first_name": "Kruparth",
+    "last_name": "Patel",
+    "age": "31-35",
+    "skill": "Wicket-keeper",
+    "batting_skill": 7,
+    "bowling_skill": 7,
+    "fielding_skill": 1,
+    "image": "public/models/2265064747.jpg"
+  },
+  {
+    "first_name": "Nisarg",
+    "last_name": "Goswami",
+    "age": "26-30",
+    "skill": "All-rounder",
+    "batting_skill": 8,
+    "bowling_skill": 9,
+    "fielding_skill": 9,
+    "image": "public/models/2493604950.jpg"
+  },
+  {
+    "first_name": "Ved",
+    "last_name": "Patel",
+    "age": "18-20",
+    "skill": "Wicket-keeper",
+    "batting_skill": 7,
+    "bowling_skill": 7,
+    "fielding_skill": 9,
+    "image": "public/models/2493776020.jpg"
+  },
+  {
+    "first_name": "Jeel",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 9,
+    "bowling_skill": 10,
+    "fielding_skill": 1,
+    "image": "public/models/2493779608.jpg"
+  },
+  {
+    "first_name": "Dutt",
+    "last_name": "Trivedi",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 7,
+    "bowling_skill": 9,
+    "fielding_skill": 8,
+    "image": "public/models/2498780723.jpg"
+  },
+  {
+    "first_name": "YUG",
+    "last_name": "PATEL",
+    "age": "18-20",
+    "skill": "All-rounder",
+    "batting_skill": 8,
+    "bowling_skill": 7,
+    "fielding_skill": 9,
+    "image": "public/models/2498853066.jpg"
+  },
+  {
+    "first_name": "Manav",
+    "last_name": "Shah",
+    "age": "21-25",
+    "skill": "Batsman",
+    "batting_skill": 7,
+    "bowling_skill": 1,
+    "fielding_skill": 1,
+    "image": "public/models/2499790010.jpg"
+  },
+  {
+    "first_name": "Sahil",
+    "last_name": "Patel",
+    "age": "31-35",
+    "skill": "All-rounder",
+    "batting_skill": 1,
+    "bowling_skill": 1,
+    "fielding_skill": 7,
+    "image": "public/models/2499790414.jpg"
+  },
+  {
+    "first_name": "Jenish",
+    "last_name": "Savaliya",
+    "age": "26-30",
+    "skill": "Wicket-keeper",
+    "batting_skill": 8,
+    "bowling_skill": 10,
+    "fielding_skill": 1,
+    "image": "public/models/2499791087.jpg"
+  },
+  {
+    "first_name": "Harsh",
+    "last_name": "vyas",
+    "age": "26-30",
+    "skill": "All-rounder",
+    "batting_skill": 10,
+    "bowling_skill": 7,
+    "fielding_skill": 10,
+    "image": "public/models/2499791445.jpg"
+  },
+  {
+    "first_name": "Urvish",
+    "last_name": "Dave",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 7,
+    "bowling_skill": 10,
+    "fielding_skill": 10,
+    "image": "public/models/2499792462.jpg"
+  },
+  {
+    "first_name": "Jeet",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 9,
+    "bowling_skill": 8,
+    "fielding_skill": 10,
+    "image": "public/models/2499793086.jpg"
+  },
+  {
+    "first_name": "Kashyap",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 9,
+    "bowling_skill": 9,
+    "fielding_skill": 1,
+    "image": "public/models/2499793132.jpg"
+  },
+  {
+    "first_name": "Avinash",
+    "last_name": "Patel",
+    "age": "26-30",
+    "skill": "All-rounder",
+    "batting_skill": 8,
+    "bowling_skill": 7,
+    "fielding_skill": 7,
+    "image": "public/models/2499793345.jpg"
+  },
+  {
+    "first_name": "Smit",
+    "last_name": "Asodariya",
+    "age": "26-30",
+    "skill": "All-rounder",
+    "batting_skill": 9,
+    "bowling_skill": 7,
+    "fielding_skill": 9,
+    "image": "public/models/2499793573.jpg"
+  },
+  {
+    "first_name": "Gunjan",
+    "last_name": "Rupapara",
+    "age": "21-25",
+    "skill": "Batsman",
+    "batting_skill": 1,
+    "bowling_skill": 2,
+    "fielding_skill": 7,
+    "image": "public/models/2499793696.jpg"
+  },
+  {
+    "first_name": "Poojan",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "Batsman",
+    "batting_skill": 7,
+    "bowling_skill": 6,
+    "fielding_skill": 8,
+    "image": "public/models/2499793718.jpg"
+  },
+  {
+    "first_name": "Urvish",
+    "last_name": "Patel",
+    "age": "18-20",
+    "skill": "All-rounder",
+    "batting_skill": 9,
+    "bowling_skill": 9,
+    "fielding_skill": 9,
+    "image": "public/models/2499794024.jpg"
+  },
+  {
+    "first_name": "Ayush",
+    "last_name": "Prajapati",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 6,
+    "bowling_skill": 7,
+    "fielding_skill": 8,
+    "image": "public/models/2499794563.jpg"
+  },
+  {
+    "first_name": "Sujeet",
+    "last_name": "Zala",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 9,
+    "bowling_skill": 9,
+    "fielding_skill": 9,
+    "image": "public/models/2893221006.jpg"
+  },
+  {
+    "first_name": "Divyam",
+    "last_name": "Patel",
+    "age": "18-20",
+    "skill": "Batsman",
+    "batting_skill": 8,
+    "bowling_skill": 5,
+    "fielding_skill": 9,
+    "image": "public/models/3456452225.jpg"
+  },
+  {
+    "first_name": "Hiren",
+    "last_name": "Thummar",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 7,
+    "bowling_skill": 9,
+    "fielding_skill": 10,
+    "image": "public/models/4162786845.jpg"
+  },
+  {
+    "first_name": "Swetang",
+    "last_name": "Amin",
+    "age": "36+",
+    "skill": "All-rounder",
+    "batting_skill": 1,
+    "bowling_skill": 5,
+    "fielding_skill": 5,
+    "image": "public/models/4164339945.jpg"
+  },
+  {
+    "first_name": "Milan",
+    "last_name": "Pandya",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 8,
+    "bowling_skill": 6,
+    "fielding_skill": 8,
+    "image": "public/models/4373662722.jpg"
+  },
+  {
+    "first_name": "Vinaykumar",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 10,
+    "bowling_skill": 7,
+    "fielding_skill": 10,
+    "image": "public/models/4384398277.jpg"
+  },
+  {
+    "first_name": "Preet",
+    "last_name": "Patel",
+    "age": "26-30",
+    "skill": "All-rounder",
+    "batting_skill": 1,
+    "bowling_skill": 7,
+    "fielding_skill": 8,
+    "image": "public/models/5195917189.jpg"
+  },
+  {
+    "first_name": "Saurin",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "Wicket-keeper",
+    "batting_skill": 10,
+    "bowling_skill": 10,
+    "fielding_skill": 10,
+    "image": "public/models/5485776177.jpg"
+  },
+  {
+    "first_name": "Nikunj",
+    "last_name": "Zalavdia",
+    "age": "31-35",
+    "skill": "Batsman",
+    "batting_skill": 1,
+    "bowling_skill": 1,
+    "fielding_skill": 1,
+    "image": "public/models/5879377978.jpg"
+  },
+  {
+    "first_name": "Mitul",
+    "last_name": "Prajapati",
+    "age": "36+",
+    "skill": "Bowler",
+    "batting_skill": 7,
+    "bowling_skill": 4,
+    "fielding_skill": 7,
+    "image": "public/models/6393175409.jpg"
+  },
+  {
+    "first_name": "Vinod",
+    "last_name": "Patel",
+    "age": "36+",
+    "skill": "Batsman",
+    "batting_skill": 5,
+    "bowling_skill": 1,
+    "fielding_skill": 1,
+    "image": "public/models/6472196464.jpg"
+  },
+  {
+    "first_name": "Tejas",
+    "last_name": "Patel",
+    "age": "36+",
+    "skill": "All-rounder",
+    "batting_skill": 5,
+    "bowling_skill": 5,
+    "fielding_skill": 1,
+    "image": "public/models/6472223434.jpg"
+  },
+  {
+    "first_name": "Bhavya",
+    "last_name": "Shah",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 10,
+    "bowling_skill": 10,
+    "fielding_skill": 9,
+    "image": "public/models/6474619424.jpg"
+  },
+  {
+    "first_name": "Dev",
+    "last_name": "Thakkar",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 8,
+    "bowling_skill": 8,
+    "fielding_skill": 1,
+    "image": "public/models/6475359928.jpg"
+  },
+  {
+    "first_name": "Kevin",
+    "last_name": "Patel",
+    "age": "36+",
+    "skill": "All-rounder",
+    "batting_skill": 8,
+    "bowling_skill": 8,
+    "fielding_skill": 8,
+    "image": "public/models/6475461929.jpg"
+  },
+  {
+    "first_name": "Akshat",
+    "last_name": "Rojesara",
+    "age": "21-25",
+    "skill": "Wicket-keeper",
+    "batting_skill": 9,
+    "bowling_skill": 9,
+    "fielding_skill": 10,
+    "image": "public/models/6475644914.jpg"
+  },
+  {
+    "first_name": "Neel",
+    "last_name": "Patel",
+    "age": "26-30",
+    "skill": "Wicket-keeper",
+    "batting_skill": 6,
+    "bowling_skill": 7,
+    "fielding_skill": 1,
+    "image": "public/models/6476128793.jpg"
+  },
+  {
+    "first_name": "Yashkamal",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 5,
+    "bowling_skill": 5,
+    "fielding_skill": 10,
+    "image": "public/models/6476795538.jpg"
+  },
+  {
+    "first_name": "Samarth",
+    "last_name": "Patel",
+    "age": "18-20",
+    "skill": "All-rounder",
+    "batting_skill": 3,
+    "bowling_skill": 3,
+    "fielding_skill": 6,
+    "image": "public/models/6477212579.jpg"
+  },
+  {
+    "first_name": "Dhruv",
+    "last_name": "Modi",
+    "age": "21-25",
+    "skill": "Wicket-keeper",
+    "batting_skill": 7,
+    "bowling_skill": 4,
+    "fielding_skill": 8,
+    "image": "public/models/6478963419.jpg"
+  },
+  {
+    "first_name": "DEEP",
+    "last_name": "CHAKLASIYA",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 7,
+    "bowling_skill": 8,
+    "fielding_skill": 10,
+    "image": "public/models/6478970230.jpg"
+  },
+  {
+    "first_name": "Jash",
+    "last_name": "Motisariya",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 7,
+    "bowling_skill": 7,
+    "fielding_skill": 7,
+    "image": "public/models/7052071270.jpg"
+  },
+  {
+    "first_name": "Atulbhai",
+    "last_name": "Patel",
+    "age": "36+",
+    "skill": "All-rounder",
+    "batting_skill": 7,
+    "bowling_skill": 9,
+    "fielding_skill": 9,
+    "image": "public/models/7052075103.jpg"
+  },
+  {
+    "first_name": "Aryan",
+    "last_name": "Amin",
+    "age": "18-20",
+    "skill": "All-rounder",
+    "batting_skill": 4,
+    "bowling_skill": 7,
+    "fielding_skill": 10,
+    "image": "public/models/7052800239.jpg"
+  },
+  {
+    "first_name": "Dhir",
+    "last_name": "Patel",
+    "age": "18-20",
+    "skill": "All-rounder",
+    "batting_skill": 6,
+    "bowling_skill": 4,
+    "fielding_skill": 7,
+    "image": "public/models/7055075569.jpg"
+  },
+  {
+    "first_name": "Mitul",
+    "last_name": "Patel",
+    "age": "18-20",
+    "skill": "All-rounder",
+    "batting_skill": 1,
+    "bowling_skill": 1,
+    "fielding_skill": 1,
+    "image": "public/models/7055078423.jpg"
+  },
+  {
+    "first_name": "OHM",
+    "last_name": "PATEL",
+    "age": "18-20",
+    "skill": "All-rounder",
+    "batting_skill": 9,
+    "bowling_skill": 9,
+    "fielding_skill": 6,
+    "image": "public/models/7056623391.jpg"
+  },
+  {
+    "first_name": "Ketanbhai",
+    "last_name": "Vyas",
+    "age": "36+",
+    "skill": "Batsman",
+    "batting_skill": 7,
+    "bowling_skill": 6,
+    "fielding_skill": 1,
+    "image": "public/models/7056655030.jpg"
+  },
+  {
+    "first_name": "Kalpeshbhai",
+    "last_name": "Patel",
+    "age": "36+",
+    "skill": "Batsman",
+    "batting_skill": 5,
+    "bowling_skill": 1,
+    "fielding_skill": 5,
+    "image": "public/models/7057067350.jpg"
+  },
+  {
+    "first_name": "Prashant",
+    "last_name": "Padashala",
+    "age": "36+",
+    "skill": "All-rounder",
+    "batting_skill": 6,
+    "bowling_skill": 7,
+    "fielding_skill": 4,
+    "image": "public/models/7058228103.jpg"
+  },
+  {
+    "first_name": "Abhay",
+    "last_name": "Lad",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 1,
+    "bowling_skill": 7,
+    "fielding_skill": 1,
+    "image": "public/models/7059180601.jpg"
+  },
+  {
+    "first_name": "Vrajkumar",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 1,
+    "bowling_skill": 1,
+    "fielding_skill": 1,
+    "image": "public/models/7059195702.jpg"
+  },
+  {
+    "first_name": "KIRTAN",
+    "last_name": "PATEL",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 1,
+    "bowling_skill": 10,
+    "fielding_skill": 10,
+    "image": "public/models/7059208017.jpg"
+  },
+  {
+    "first_name": "Meet",
+    "last_name": "Kapatel",
+    "age": "26-30",
+    "skill": "All-rounder",
+    "batting_skill": 10,
+    "bowling_skill": 1,
+    "fielding_skill": 10,
+    "image": "public/models/7059216417.jpg"
+  },
+  {
+    "first_name": "Jimmy",
+    "last_name": "Rathod",
+    "age": "36+",
+    "skill": "Batsman",
+    "batting_skill": 5,
+    "bowling_skill": 5,
+    "fielding_skill": 1,
+    "image": "public/models/7059219281.jpg"
+  },
+  {
+    "first_name": "Nirav",
+    "last_name": "Vagadiya",
+    "age": "26-30",
+    "skill": "All-rounder",
+    "batting_skill": 9,
+    "bowling_skill": 7,
+    "fielding_skill": 9,
+    "image": "public/models/7059232654.jpg"
+  },
+  {
+    "first_name": "Niyam",
+    "last_name": "Patel",
+    "age": "18-20",
+    "skill": "Bowler",
+    "batting_skill": 1,
+    "bowling_skill": 10,
+    "fielding_skill": 7,
+    "image": "public/models/7059232799.jpg"
+  },
+  {
+    "first_name": "Shivang",
+    "last_name": "Darji",
+    "age": "21-25",
+    "skill": "Batsman",
+    "batting_skill": 5,
+    "bowling_skill": 5,
+    "fielding_skill": 5,
+    "image": "public/models/7059235598.jpg"
+  },
+  {
+    "first_name": "SHALIN",
+    "last_name": "PATEL",
+    "age": "18-20",
+    "skill": "Wicket-keeper",
+    "batting_skill": 1,
+    "bowling_skill": 9,
+    "fielding_skill": 1,
+    "image": "public/models/7059292058.jpg"
+  },
+  {
+    "first_name": "kantubhai",
+    "last_name": "Patel",
+    "age": "36+",
+    "skill": "All-rounder",
+    "batting_skill": 5,
+    "bowling_skill": 5,
+    "fielding_skill": 7,
+    "image": "public/models/7059380403.jpg"
+  },
+  {
+    "first_name": "keval",
+    "last_name": "Parmar",
+    "age": "21-25",
+    "skill": "Batsman",
+    "batting_skill": 1,
+    "bowling_skill": 1,
+    "fielding_skill": 5,
+    "image": "public/models/7059753746.jpg"
+  },
+  {
+    "first_name": "Siddharth",
+    "last_name": "Malavi",
+    "age": "26-30",
+    "skill": "Batsman",
+    "batting_skill": 10,
+    "bowling_skill": 6,
+    "fielding_skill": 10,
+    "image": "public/models/7059784519.jpg"
+  },
+  {
+    "first_name": "Bhavin",
+    "last_name": "Vavadiya",
+    "age": "31-35",
+    "skill": "All-rounder",
+    "batting_skill": 9,
+    "bowling_skill": 10,
+    "fielding_skill": 10,
+    "image": "public/models/7059880911.jpg"
+  },
+  {
+    "first_name": "Abhi",
+    "last_name": "Patel",
+    "age": "26-30",
+    "skill": "Wicket-keeper",
+    "batting_skill": 7,
+    "bowling_skill": 7,
+    "fielding_skill": 10,
+    "image": "public/models/7059882603.jpg"
+  },
+  {
+    "first_name": "Neel",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 7,
+    "bowling_skill": 10,
+    "fielding_skill": 10,
+    "image": "public/models/7059883049.jpg"
+  },
+  {
+    "first_name": "Maulik",
+    "last_name": "Patel",
+    "age": "31-35",
+    "skill": "Batsman",
+    "batting_skill": 6,
+    "bowling_skill": 1,
+    "fielding_skill": 8,
+    "image": "public/models/7059885150.jpg"
+  },
+  {
+    "first_name": "MAULIK",
+    "last_name": "LIMBACHIYA",
+    "age": "31-35",
+    "skill": "Batsman",
+    "batting_skill": 5,
+    "bowling_skill": 5,
+    "fielding_skill": 5,
+    "image": "public/models/7059885531.jpg"
+  },
+  {
+    "first_name": "Purva",
+    "last_name": "Patel",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 6,
+    "bowling_skill": 6,
+    "fielding_skill": 1,
+    "image": "public/models/7059886257.jpg"
+  },
+  {
+    "first_name": "Jainish",
+    "last_name": "Raval",
+    "age": "21-25",
+    "skill": "All-rounder",
+    "batting_skill": 5,
+    "bowling_skill": 5,
+    "fielding_skill": 5,
+    "image": "public/models/7059886978.jpg"
+  },
+  {
+    "first_name": "Dhruv",
+    "last_name": "Kapatel",
+    "age": "26-30",
+    "skill": "All-rounder",
+    "batting_skill": 10,
+    "bowling_skill": 10,
+    "fielding_skill": 10,
+    "image": "public/models/7059888109.jpg"
+  },
+  {
+    "first_name": "Hardik",
+    "last_name": "Ghevariya",
+    "age": "21-25",
+    "skill": "Batsman",
+    "batting_skill": 5,
+    "bowling_skill": 1,
+    "fielding_skill": 5,
+    "image": "public/models/7059888214.jpg"
+  },
+  {
+    "first_name": "Rajni",
+    "last_name": "Ghevariya",
+    "age": "26-30",
+    "skill": "Batsman",
+    "batting_skill": 6,
+    "bowling_skill": 5,
+    "fielding_skill": 6,
+    "image": "public/models/7059888225.jpg"
+  },
+  {
+    "first_name": "DARSHIL",
+    "last_name": "RIJIYA",
+    "age": "26-30",
+    "skill": "All-rounder",
+    "batting_skill": 8,
+    "bowling_skill": 7,
+    "fielding_skill": 8,
+    "image": "public/models/9029896857.jpg"
+  },
+  {
+    "first_name": "Mitesh",
+    "last_name": "Patel",
+    "age": "36+",
+    "skill": "All-rounder",
+    "batting_skill": 7,
+    "bowling_skill": 7,
+    "fielding_skill": 7,
+    "image": "public/models/9053270163.jpg"
+  },
+  {
+    "first_name": "Het",
+    "last_name": "Patel",
+    "age": "18-20",
+    "skill": "All-rounder",
+    "batting_skill": 1,
+    "bowling_skill": 8,
+    "fielding_skill": 5,
+    "image": "public/models/9059335814.jpg"
+  }
+]
